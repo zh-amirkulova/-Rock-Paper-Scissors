b0bde1add04f56ef63ec7fda461fc13a786a40cf
Zhanerke <zhanerke.amirkulova@gmail.com>
2025-01-22 19:31:20 +0500

    Wrote the logic to get the human choice

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
-console.log('hello world');
+//get the computer choice
 
 function getComputerChoice (min, max) {
-  min = Math.ceil(min); // Округление вверх
-  max = Math.floor(max); // Округление вниз
+  min = Math.ceil(min); 
+  max = Math.floor(max); 
   return Math.floor(Math.random() * (max - min + 1)) + min;
+  
 }
 
-// console.log(getComputerChoice (1, 3));
-
 let result =  getComputerChoice (1, 3)
-
-console.log(result)
-
 
 if (result===1){
   console.log('rock');
 } else if (result===2) {
   console.log('paper');
 } else console.log('scissors');
+
+
+// get the human choice
+
+const num = prompt('To play, enter the number (1="rock", 2="paper", 3="scissors"): ');
+const parsedNumber = Number(num);
+
+function getHumanChoice(parsedNumber){
+    if (parsedNumber===1){
+    console.log('rock');
+  } else if (parsedNumber===2) {
+    console.log('paper');
+  } else if  (parsedNumber===3){
+    console.log('scissors');
+  } else console.log('You entered the wrong value!');
+}
+
+getHumanChoice(parsedNumber);
+
+
+  
