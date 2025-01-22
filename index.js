d521cae149032e70203795a9ded87e5357b0a9c1
Zhanerke <zhanerke.amirkulova@gmail.com>
2025-01-22 18:50:45 +0500

    Wrote the logic to get the computer choice

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 console.log('hello world');
+
+function getComputerChoice (min, max) {
+  min = Math.ceil(min); // Округление вверх
+  max = Math.floor(max); // Округление вниз
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// console.log(getComputerChoice (1, 3));
+
+let result =  getComputerChoice (1, 3)
+
+console.log(result)
+
+
+if (result===1){
+  console.log('rock');
+} else if (result===2) {
+  console.log('paper');
+} else console.log('scissors');
