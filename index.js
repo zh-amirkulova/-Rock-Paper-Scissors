9d1bca2715d606402aaf664a885ff7e0f4c4f53e
Zhanerke <zhanerke.amirkulova@gmail.com>
2025-01-23 07:57:58 +0500

    Completed game logic for Rock-Paper-Scissors

--- index.js
+++ index.js
@@ -1,123 +1,57 @@
-// get the computer choice
+let humanScore = 0;
+let computerScore = 0;
 
-// function getComputerChoice (min, max) {
-//   min = Math.ceil(min); 
-//   max = Math.floor(max); 
-//   return Math.floor(Math.random() * (max - min + 1)) + min
-  
-// }
-
-// const result =  getComputerChoice (1, 3)
-
-// if (result===1){
-//   console.log('Computer: rock');
-// } else if (result===2) {
-//   console.log('Computer: paper');
-// } else console.log('Computer: scissors');
-
-
-// get the human choice
-
-// const num = prompt('To play, enter the number (1="rock", 2="paper", 3="scissors"): ');
-// const parsedNumber = Number(num);
-
-// function getHumanChoice(parsedNumber){
-//     if (parsedNumber===1){
-//     console.log('You: rock');
-//   } else if (parsedNumber===2) {
-//     console.log('You: paper');
-//   } else if  (parsedNumber===3){
-//     console.log('You: scissors');
-//   } else console.log('You entered the wrong value!');
-// }
-
-// getHumanChoice(parsedNumber);
-
-
-// // the players score variables
-// let humanScore = 0;
-// let computerScore = 0;
-
-// // the logic to play a single round
-// function playRound(humanChoice, computerChoice){
-//   if (humanChoice === 1 && computerChoice === 2 || humanChoice === 2 && computerChoice === 3 || 
-//       humanChoice === 3 && computerChoice === 1
-//   ){
-//     console.log("You lose!")
-//     return computerScore++;
-//   } else if (humanChoice === 1 && computerChoice === 3 || humanChoice === 2 && computerChoice === 1 ||
-//              humanChoice === 3 && computerChoice === 2
-//    ){
-//     console.log("You  won!")
-//     return humanScorу++;
-//   } else console.log("Draw!")
-//   return;
-  
-// };
-
-// const humanSelection  = parsedNumber;
-// const computerSelection  = result;
-
-// playRound(humanSelection, computerSelection);
-
+playGame()
 
 function playGame(){
   for (let i=0; i<5; i++){
+      const humanSelection  = getHumanChoice();
+      const computerSelection  = getComputerChoice (1, 3);
 
-    function getComputerChoice (min, max) {
-      min = Math.ceil(min); 
-      max = Math.floor(max); 
-      return Math.floor(Math.random() * (max - min + 1)) + min
-      
-    }
-    
-    const result =  getComputerChoice (1, 3)
-    
-    if (result===1){
-      console.log('Computer: rock');
-    } else if (result===2) {
-      console.log('Computer: paper');
-    } else console.log('Computer: scissors');
+      playRound(humanSelection, computerSelection);
+      console.log('Human score:',humanScore, 'Computer score:',computerScore);
+  }
 
-    const num = prompt('To play, enter the number (1="rock", 2="paper", 3="scissors"): ');
-    const parsedNumber = Number(num);
-
-    function getHumanChoice(parsedNumber){
-        if (parsedNumber===1){
-        console.log('You: rock');
-      } else if (parsedNumber===2) {
-        console.log('You: paper');
-      } else if  (parsedNumber===3){
-        console.log('You: scissors');
-      } else alert('You entered the wrong value!');
-    }
-    // the players score variables
-    let humanScore = 0;
-    let computerScore = 0;
-
-    // the logic to play a single round
-    function playRound(humanChoice, computerChoice){
-      if (humanChoice === 1 && computerChoice === 2 || humanChoice === 2 && computerChoice === 3 || 
-          humanChoice === 3 && computerChoice === 1
-      ){
-        alert("You lose this round!");
-        return computerScore++;
-      } else if (humanChoice === 1 && computerChoice === 3 || humanChoice === 2 && computerChoice === 1 ||
-                humanChoice === 3 && computerChoice === 2
-      ){
-        alert("You won this round!");
-        return humanScore++;
-      } else alert("Drow this round!");
-      return;
-      
-    };
-    const humanSelection  = parsedNumber;
-    const computerSelection  = result;
-
-    getHumanChoice(parsedNumber);
-    playRound(humanSelection, computerSelection);
-    console.log(humanScore,computerScore);
-  };
+  if (humanScore>computerScore){
+    alert('You are the winner!');
+    console.log('Human is the winnner!');
+  } else if (humanScore<computerScore){
+    alert('You lost!');
+    console.log('Computer is the winnner!');
+  } else if(humanScore===computerScore){
+    alert('Draw!');
+    console.log('Was drow!');
+  }
 };
 
-playGame(); 
+// get the computer choice
+function getComputerChoice (min, max) {
+min = Math.ceil(min); 
+max = Math.floor(max); 
+return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+// get the human choice
+function getHumanChoice(){
+  const num = prompt('To play, enter the number (1="rock", 2="paper", 3="scissors"): ');
+  const parsedNumber = Number(num);
+  return parsedNumber;
+};
+
+// the logic to play a single round
+function playRound(humanChoice, computerChoice){
+  if (humanChoice === 1 && computerChoice === 2 || humanChoice === 2 && computerChoice === 3 || 
+      humanChoice === 3 && computerChoice === 1
+  ){
+      alert("You lose this round!");
+      return computerScore++;
+  } else if (humanChoice === 1 && computerChoice === 3 || humanChoice === 2 && computerChoice === 1 ||
+              humanChoice === 3 && computerChoice === 2
+  ){
+      alert("You  won this round!");
+      return humanScore++;
+  } else alert("Draw this round!")
+  return;
+
+};
